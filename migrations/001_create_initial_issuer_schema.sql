f47ed6fc1a524e05970036f0768b4dbf8edf00d6
Leonardo Freitas <lfreitas.lpf@gmail.com>
2026-07-27 16:56:37 -0300

    chore: add initial issuer schema migration

---
+++ migrations/001_create_initial_issuer_schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS issuer
+(
+    id           BLOB PRIMARY KEY,
+    name         TEXT,
+    status       TEXT    NOT NULL CHECK (status IN ('ACTIVE', 'RETIRED')),
+    created_at   TEXT    NOT NULL,
+    version      INTEGER NOT NULL DEFAULT 1,
+    cnpj         TEXT UNIQUE,
+    lei          TEXT UNIQUE,
+    country_code TEXT CHECK (country_code IS NULL OR length(country_code) = 2)
+) STRICT, WITHOUT ROWID;
