df5cdd86963735a0e430d609a027cbf183535c9d
Leonardo Freitas <lfreitas.lpf@gmail.com>
2026-08-23 16:36:25 -0300

    chore: update database initial migration and remove old code

--- crates/infrastructure/src/sqlite/mod.rs
+++ crates/infrastructure/src/sqlite/mod.rs
@@ -6,7 +6,6 @@ mod migrations;
 mod row;
 mod security;
 mod support;
-mod task;
 
 pub use crate::sqlite::database::{DatabaseConfig, Synchronous};
 pub use crate::sqlite::engine::SqliteStorageEngine;
