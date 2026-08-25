59ee4ab608bc00089fc401e35cc4bcf1df3042af
Leonardo Freitas <lfreitas.lpf@gmail.com>
2026-08-25 18:25:53 -0300

    refactor: simplify `Display` implementation for `UniqueIdentifier`

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -53,7 +53,7 @@ impl From<Uuid> for UniqueIdentifier {
 
 impl Display for UniqueIdentifier {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0.to_string())
+        write!(f, "{}", self.0)
     }
 }
 
