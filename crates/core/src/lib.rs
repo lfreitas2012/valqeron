b7b4a768432f5c1dcb7ce4abb02aeb707ea18089
Leonardo Freitas <lfreitas.lpf@gmail.com>
2026-08-30 20:43:59 -0300

    chore: update core lib imports and reorganize modules

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,17 +1,14 @@
 mod background_tasks;
-mod domain;
-mod identifiers;
 mod storage;
 
 pub mod common;
+pub mod domain;
+pub mod identifiers;
 
 pub use storage::{PersistenceManager, Repositories, StorageEngine, StorageError, StorageFault};
 
 pub use valqeron_identifiers::{
-    Cfi, CfiError, Cnpj, CnpjError, CountryCode, CountryCodeError, Isin, IsinError, Lei, LeiError,
-    Mic, MicError,
+    Cfi, CfiError, CountryCode, CountryCodeError, Isin, IsinError, Lei, LeiError, Mic, MicError,
 };
-
-pub use identifiers::{CurrencyCode, CurrencyCodeError};
 
 pub use background_tasks::BackgroundTaskSnapshot;
